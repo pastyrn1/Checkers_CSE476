f7335ed2fbb986c2696b5f1553789dadce29c1b5
jewelyan <jewelyanb@gmail.com>
2020-10-06 17:52:50 -0500

    Update Game.java
    Green play screen, and small square for checkered board

--- proj1/app/src/main/java/edu/msu/lukshani/proj1/Game.java
+++ proj1/app/src/main/java/edu/msu/lukshani/proj1/Game.java
@@ -1,12 +1,12 @@
 package edu.msu.lukshani.proj1;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
+//import android.graphics.Bitmap;
+//import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.Shader;
+//import android.graphics.Rect;
+//import android.graphics.Shader;
 
 public class Game {
     /**
@@ -14,7 +14,12 @@ public class Game {
      */
     private Paint fillPaint;
 
-    private BitmapShader shader;
+    /**
+     * * paint for the square tiles
+     */
+    private Paint fillPaintSec;
+
+    //private BitmapShader shader;
 
 
 
@@ -22,7 +27,7 @@ public class Game {
      * Percentage of the display width or height that
      * is occupied by the puzzle.
      */
-    final static float SCALE_IN_VIEW = 0.9f;
+    final static float SCALE_IN_VIEW = 1;
     
     public Game(Context context){
 
@@ -31,32 +36,39 @@ public class Game {
         fillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         fillPaint.setColor(0xff006400);
 
+        fillPaintSec = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fillPaintSec.setColor(0xffDCDCDC);
+        //fillPaint.setStyle(Paint.Style.FILL);
+
+        //Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
     }
 
     public void draw(Canvas canvas){
 
-        int wid = 2;
-        int hit = 2;
+        //int wid = 2;
+        //int hit = 2;
 
-        int pixelSize = wid * hit;
+       // int pixelSize = wid * hit;
 
-        Bitmap bitmap = Bitmap.createBitmap(pixelSize * 2, pixelSize * 2, Bitmap.Config.ARGB_8888);
+        //Bitmap bitmap = Bitmap.createBitmap(pixelSize * 2, pixelSize * 2, Bitmap.Config.ARGB_8888);
 
 
-        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
-        Paint paint = new Paint();
-        paint.setShader(shader);
+        //BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        //Paint paint = new Paint();
+        //paint.setShader(shader);
 
-        //int wid = canvas.getWidth();
-        //int hit = canvas.getHeight();
+        int wid = canvas.getWidth();
+        int hit = canvas.getHeight();
+
 
         // Determine the minimum of the two dimensions
-        //int minDim = wid < hit ? wid : hit;
+        int minDim = wid < hit ? wid : hit;
 
-        //int pixelSize = (int)(minDim * SCALE_IN_VIEW);
+        int pixelSize = (int)(minDim * SCALE_IN_VIEW);
 
-        new Canvas(bitmap);
-        Rect rect = new Rect(0, 0, pixelSize, pixelSize);
+        //new Canvas(bitmap);
+        //Rect rect = new Rect(0, 0, pixelSize, pixelSize);
         //canvas.drawRect(rect, fillPaint);
         //rect.offset(pixelSize, pixelSize);
         //canvas.drawRect(rect, fillPaint);
@@ -67,12 +79,23 @@ public class Game {
         int marginX = (wid - pixelSize) / 2;
         int marginY = (hit - pixelSize) / 2;
 
+        int sec_wid = 100;
+        int sec_hit = 100;
+
+        int offsetX = (sec_wid - pixelSize) / 2;
+        int offsetY = (sec_hit - pixelSize) / 2;
+
+
         //
         // Draw the outline of the puzzle
         //
 
-        canvas.drawRect(pixelSize, pixelSize,
-                 pixelSize,  pixelSize, fillPaint);
+        canvas.drawRect(marginX, marginY,
+                 marginX+ pixelSize,  marginY + pixelSize, fillPaint);
+
+        canvas.drawRect(offsetX, offsetY, sec_hit,  sec_wid, fillPaintSec);
+
+       // return paint;
 
     }
 }
