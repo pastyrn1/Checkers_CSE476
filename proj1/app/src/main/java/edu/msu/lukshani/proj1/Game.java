f983ea5884cf6a0e1d90479b4fcdd319d0ae923a
jewelyan <jewelyanb@gmail.com>
2020-10-06 16:08:14 -0500

    Game activity, game view added
    Working on drawing background

---
+++ proj1/app/src/main/java/edu/msu/lukshani/proj1/Game.java
@@ -0,0 +1,78 @@
+package edu.msu.lukshani.proj1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+
+public class Game {
+    /**
+     * Paint for filling the area the puzzle is in
+     */
+    private Paint fillPaint;
+
+    private BitmapShader shader;
+
+
+
+    /**
+     * Percentage of the display width or height that
+     * is occupied by the puzzle.
+     */
+    final static float SCALE_IN_VIEW = 0.9f;
+    
+    public Game(Context context){
+
+        // Create paint for filling the area the puzzle will
+        // be solved in.
+        fillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fillPaint.setColor(0xff006400);
+
+    }
+
+    public void draw(Canvas canvas){
+
+        int wid = 2;
+        int hit = 2;
+
+        int pixelSize = wid * hit;
+
+        Bitmap bitmap = Bitmap.createBitmap(pixelSize * 2, pixelSize * 2, Bitmap.Config.ARGB_8888);
+
+
+        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        Paint paint = new Paint();
+        paint.setShader(shader);
+
+        //int wid = canvas.getWidth();
+        //int hit = canvas.getHeight();
+
+        // Determine the minimum of the two dimensions
+        //int minDim = wid < hit ? wid : hit;
+
+        //int pixelSize = (int)(minDim * SCALE_IN_VIEW);
+
+        new Canvas(bitmap);
+        Rect rect = new Rect(0, 0, pixelSize, pixelSize);
+        //canvas.drawRect(rect, fillPaint);
+        //rect.offset(pixelSize, pixelSize);
+        //canvas.drawRect(rect, fillPaint);
+
+
+
+        // Compute the margins so we center the puzzle
+        int marginX = (wid - pixelSize) / 2;
+        int marginY = (hit - pixelSize) / 2;
+
+        //
+        // Draw the outline of the puzzle
+        //
+
+        canvas.drawRect(pixelSize, pixelSize,
+                 pixelSize,  pixelSize, fillPaint);
+
+    }
+}
---
+++ proj1/app/src/main/java/edu/msu/lukshani/proj1/GameView.java
@@ -0,0 +1,57 @@
+package edu.msu.lukshani.proj1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * Custom view class for our Game.
+ */
+public class GameView extends View {
+
+    /**
+     * Percentage of the display width or height that
+     * is occupied by the puzzle.
+     */
+    //final static float SCALE_IN_VIEW = 0.9f;
+
+    /**
+     * The game
+     */
+    private Game game;
+
+
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        game = new Game(getContext());
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        game.draw(canvas);
+
+    }
+
+
+}
