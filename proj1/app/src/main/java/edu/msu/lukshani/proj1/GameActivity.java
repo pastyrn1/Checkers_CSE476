fad3aa94fbfda6b4a1464da3074a5d87c823d237
lukshani <lukshani@msu.edu>
2020-10-20 19:03:37 -0400

    UserInput default and player1 displayed

--- proj1/app/src/main/java/edu/msu/lukshani/proj1/GameActivity.java
+++ proj1/app/src/main/java/edu/msu/lukshani/proj1/GameActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView tv;
+    String st;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        tv = findViewById(R.id.textView);
+
+        st = getIntent().getExtras().getString("player");
+        tv.setText(st);
     }
 
     public void onResign(View View) {
--- proj1/app/src/main/java/edu/msu/lukshani/proj1/OpenActivity.java
+++ proj1/app/src/main/java/edu/msu/lukshani/proj1/OpenActivity.java
@@ -12,30 +12,37 @@ public class OpenActivity extends AppCompatActivity {
 
     String name;
     EditText nameInput;
+    EditText nameInput2;
+    Button button;
 
-    Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
 
+        button = findViewById(R.id.startButton);
+        nameInput = findViewById(R.id.player1name);
+        nameInput2 = findViewById(R.id.player2name);
 
-        nameInput = (EditText)findViewById(R.id.player1name);
 
-//        startButton = (Button)findViewById(R.id.startButton);
-//        startButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                name = nameInput.getText().toString();
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(OpenActivity.this, GameActivity.class);
+                name = nameInput.getText().toString();
+                i.putExtra("player", name);
+                startActivity(i);
+                finish();
+            }
+        });
 
+        nameInput.setText(R.string.player1text);
+        nameInput2.setText(R.string.player2text);
 
     }
 
-
-            public void onStart(View view) {
-                startActivity(new Intent(OpenActivity.this, GameActivity.class));
-            }
+//            public void onStart(View view) {
+//                startActivity(new Intent(OpenActivity.this, GameActivity.class));
+//            }
 
 }
