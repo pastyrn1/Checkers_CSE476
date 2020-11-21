37efe64fb6835b874fa65be3870c513c5916b282
jewelyan <jewelyanb@gmail.com>
2020-11-21 07:35:17 -0600

    Create User.java

---
+++ proj1/app/src/main/java/edu/msu/lukshani/proj1/User.java
@@ -0,0 +1,23 @@
+package edu.msu.lukshani.proj1;
+
+public class User {
+    private static String MAGIC = "";
+    private static String USER = "";
+    private static String PASSWORD = "";
+
+    public static String getMAGIC() {
+        return MAGIC;
+    }
+
+    public static String getPassword(){
+        return PASSWORD;
+    }
+
+    public static String User(){;
+        {
+            return USER;
+            
+        }
+    }
+
+}
