a0ffadc159c589f091dc079858d9771678ded413
Anna <pastyrn1@msu.edu>
2020-11-23 18:24:11 -0500

    Create Cloud.java

---
+++ proj2/proj1/app/src/main/java/edu/msu/lukshani/proj1/Cloud/Cloud.java
@@ -0,0 +1,72 @@
+package edu.msu.lukshani.proj1.Cloud;
+
+import android.util.Log;
+import android.util.Xml;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+
+//import edu.msu.lukshani.proj1.Cloud.R;
+//import retrofit2.Response;
+//import retrofit2.Retrofit;
+//import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
+@SuppressWarnings("deprecation")
+public class Cloud {
+    private static final String BASE_URL = "https://webdev.cse.msu.edu/~pastyrn1/cse476/project2/";
+    public static final String CATALOG_PATH = "create-player.php";
+
+    private static final String MAGIC = "NechAtHa6RuzeR8x";
+
+    //private static final String USER = "pastyrn1";
+    //private static final String PASSWORD = "ThankYOU";
+
+    /*private static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .build();*/
+
+    /**
+     *
+     *//*
+    public static class UserAdapter extends BaseAdapter {
+
+        *//**
+         * Constructor
+         *//*
+        public UserAdapter(final View view) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Item getItem(int position) {
+            return 0;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+           return view;
+        }
+
+    }*/
+
+}
