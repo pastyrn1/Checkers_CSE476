8fdc6283d042251846a43423bb2251921f60acca
lukshani <lukshani@msu.edu>
2020-11-23 17:06:56 -0500

    update CreateActivity

--- proj2/proj1/app/src/main/java/edu/msu/lukshani/proj1/CreateActivity.java
+++ proj2/proj1/app/src/main/java/edu/msu/lukshani/proj1/CreateActivity.java
@@ -34,19 +34,23 @@ public class CreateActivity extends AppCompatActivity {
                 String pass1 = password1.getText().toString();
                 String pass2 = password2.getText().toString();
 
-                if(pass1 != pass2){
+                if(!pass1.equals(pass2)){
                     password2.setError("Passwords do not not match");
                     //Toast.makeText(CreateActivity.this, "Passwords don't match", Toast.LENGTH_LONG).show();
+                    return;
                 }
                 else{ // both passwords are the same
                     //String pass = pass1;
                     if(TextUtils.isEmpty(user)){
                         username.setError("Please provide username");
+                        return;
 //                        Toast.makeText(CreateActivity.this, "Missing UserId or Password", Toast.LENGTH_LONG).show();
                     }else if(TextUtils.isEmpty(pass1)){
                         password1.setError("Please provide password");
+                        return;
                     }else if(TextUtils.isEmpty(pass2)){
                         password2.setError("Please confirm password");
+                        return;
                     }//create and save
 
                 }
